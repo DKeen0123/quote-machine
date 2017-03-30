124e5959b04427b53685b39d33b09b1c37405d07
Daniel <keendaniel01@gmail.com>
2017-03-30 17:54:54 +0100

    Completed javascript for quote button.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,28 @@
-import RandomQuote from './modules/RandomQuote';
 import $ from 'jquery';
 
+function randomQuote() {
+$.ajax({
+    url: "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?",
+dataType: "json",
+data: "method=getQuote&format=json&lang=en",
+    success: function(data) {
+      $("#quote").html("<p id='quote' class='lead text-center'>" + data.quoteText);
+      $("#author").html("<p id='author'>" + "<br/>&dash; " + data.quoteAuthor + "&dash;</p>" );
+    }
+
+});
+}
+
+$(function() {
+randomQuote();
+});
+
 $(".button__btn").click(function(){
-  randomQuote();
+randomQuote();
+});
+
+$('#tweet').on("click", function(){
+     var ranQuote = $('#quote').text();
+     var enCode = encodeURI(ranQuote);
+  window.open("https://twitter.com/intent/tweet?text="+ enCode)
 });
